848848a09d1e140cf13ef4f6df761eea12d71e98
Afreed7411 <135531217+Afreed7411@users.noreply.github.com>
2023-12-25 20:52:41 +0530

    Add files via upload
    this is TIcTacToe app #react js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Tictactoe from './components/TicTacToe/Tictactoe';
+
+function App() {
+  return (
+    <div>
+     <Tictactoe/>
+    </div>
+  );
+}
+
+export default App;
